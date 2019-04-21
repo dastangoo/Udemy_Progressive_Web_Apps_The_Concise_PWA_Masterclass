d109150ffd7a0ba5bb815867d9e20ecac6ca483a
dastangoo <hamed.dastangoo@gmail.com>
2019-04-21 20:15:42 +0430

    Add some examples

--- main.js
+++ main.js
@@ -4,19 +4,10 @@
 if (navigator.serviceWorker) {
 
   // Register the SW
-  navigator.serviceWorker.register('./sw.js').then(function(registration){
-
+  navigator.serviceWorker.register('./sw.js', {scope: '/posts/'}).then(function(registration){
     console.log('SW Registered');
 
   }).catch(console.log);
 
 }
 
-// Get camera feed
-fetch('camera_feed.html')
-.then((res) => {
-    return res.text();
-})
-.then((html) => {
-   document.getElementById('camera').innerHTML = html;
-})
--- sw.js
+++ sw.js
@@ -1,19 +1,4 @@
 // Service Worker
-
-self.addEventListener('fetch', (e) => {
-    //if (e.request.url.endsWith('style.css')) {
-       //e.respondWith(fetch('./style2.css'))
-    //}
-    //let headers = new Headers({'Content-Type': 'text/html'});
-    //let customRes = new Response('<h1>Hello World</h1>', {headers});
-    //e.respondWith(customRes);
-    if (e.request.url.endsWith('/camera_feed.html')) {
-        e.respondWith(
-            fetch(e.request)
-                .then((res) => {
-                   if (res.ok) return res;
-                   return new Response('Camera feed currently not available.');
-                })
-        )  
-    }      
+self.addEventListener('activate', () => {
+    console.log('SW Active');
 });
