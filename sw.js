2f184b110dcf44789812861c567d65bee12f8bfa
dastangoo <hamed.dastangoo@gmail.com>
2019-04-23 19:44:19 +0430

    Add some examples

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
 // Service Worker
 
-self.addEventListener('message', (e) => {
-    console.log(e);
-    //if (e.data === "update_self") {
-        //console.log('Service Worker Updating');    
-        //self.skipWaiting(); 
-    //}
-
-    // Respond to all clients
-    self.clients.matchAll().then((clients) => {
-        //console.log(clients);
-        clients.forEach((client) => {
-            //client.postMessage("Hello from Service Worker");    
-            if (e.source.id === client.id) {
-                client.postMessage("Private Hello from Service Worker");    
-            }
-        });
-        
-        // Only respond to sending client
-    })
+self.addEventListener('push', () => {
+    console.log('Push Received');
 })
