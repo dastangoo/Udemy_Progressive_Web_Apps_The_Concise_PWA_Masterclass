f054e9fdcc467c572212a3b272f0c20629a4dd2d
dastangoo <hamed.dastangoo@gmail.com>
2019-04-29 19:45:55 +0430

    Add some examples

--- main.js
+++ main.js
@@ -1,31 +1,20 @@
 
 // Progressive Enhancement (SW supported)
 // if ('serviceWorker' in navigator) {
-if (navigator.serviceWorker) {
+//if (navigator.serviceWorker) {
 
-  // Convert key to Unit8Array
-  function urlBase64ToUnit8Array (base64String) {
-      const padding = '='.repeat((4 - base64String.length % 4) % 4);
-      const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-  }
   // Register the SW
-  navigator.serviceWorker.register('./sw.js').then(function(registration){
-      // Server public key  
-      let pubKey = 'BI2aS6OoUaEXJwayEggXPftVSC6DcZtaFuNkNgKRUGO7v92VO3s-iVhPsGJRwNMiyIn9WDut3n5c8N7IN01L_FM';
-
-      registration.pushManager.getSubscription().then((sub) => {
-          // If subscription found, return
-          if (sub) return sub;
-
-          let applicationServerKey = urlBase64ToUnit8Array(pubKey);
-
-          // Subscribe
-          return registration.pushManager.subscribe({userVisibleOnly: true, applicationServerKey});
-      }).then( sub => sub.toJSON() )
-        .then(console.log)
-        .catch(console.log);
-  }).catch(console.log);
+  //navigator.serviceWorker.register('./sw.js').then(function(registration){
+  //}).catch(console.log);
+//}
+if (window.caches) {
+    //caches.open('test1');
+    //caches.open('test2');
+    //caches.keys().then(console.log);
+    //caches.has('test1').then(console.log);
+    //caches.has('test3').then(console.log);
+    //caches.delete('test1').then(console.log);
+    caches.open('test2').then((cache) => {
+            
+    });
 }
-
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker
 
 // Send notification on push
-self.addEventListener('push', (e) => {
-    let n = self.registration.showNotification('A notification from the SW.');
-    e.waitUntil(n);
-});
+//self.addEventListener('push', (e) => {
+    //let n = self.registration.showNotification('A notification from the SW.');
+    //e.waitUntil(n);
+//});
