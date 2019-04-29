c11643badefe3da947f53989b0b27f4942d7e456
dastangoo <hamed.dastangoo@gmail.com>
2019-04-29 21:34:09 +0430

    Add some examples

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 
 // Progressive Enhancement (SW supported)
 // if ('serviceWorker' in navigator) {
-//if (navigator.serviceWorker) {
+if (navigator.serviceWorker) {
 
   // Register the SW
-  //navigator.serviceWorker.register('./sw.js').then(function(registration){
-  //}).catch(console.log);
-//}
-if (window.caches) {
-    caches.open('pwa-v1.1').then((cache) => {
-        //cache.put('index.html', new Response('My own HTML'));
-        //cache.match('/index.html').then((res) => {
-            //res.text().then(console.log);
-        //})
-        cache.keys().then(console.log);
-    });
+  navigator.serviceWorker.register('./sw.js').then(function(registration){
+  }).catch(console.log);
 }
--- sw.js
+++ sw.js
@@ -1,7 +1,51 @@
 // Service Worker
+const pwaCache = 'pwa-cache-2';
+self.addEventListener('install', (e) => {
+    let cacheReady = caches.open(pwaCache).then((cache) => {
+        console.log('New cache ready.');
+        return cache.addAll([
+            '/',
+            'style.css',
+            'thumb.png',
+            'main.js'
+        ]);
+    });
+    e.waitUntil(cacheReady);
+});
 
-// Send notification on push
-//self.addEventListener('push', (e) => {
-    //let n = self.registration.showNotification('A notification from the SW.');
-    //e.waitUntil(n);
-//});
+self.addEventListener('activate', (e) => {
+    let cacheCleaned = caches.keys().then((keys) => {
+        keys.forEach((key) => {
+            if (key !== pwaCache) {
+                return caches.delete(key);
+            }
+        });
+    });
+
+    e.waitUntil(cacheCleaned);
+});
+
+self.addEventListener('fetch', (e) => {
+    //if (e.request.url === 'http://localhost:8888/index.html') {
+    // Skip for remote fetch
+    if ( !e.request.url.match(location.origin)) return;
+    
+    // Serve local fetch form cache
+    let newRes = caches.open(pwaCache).then((cache) => {
+        return cache.match(e.request).then((res) => {
+            // Check request was found in cache
+            if (res) {
+                console.log(`Serving ${res.url} from cache.`);
+                return res;
+            }
+            
+            // Fetch on behalf of client and cache
+            return fetch(e.request).then((fetchRes) => {
+                cache.put(e.request, fetchRes.clone());
+                return fetchRes;
+            });
+        });
+    });
+
+   e.respondWith(newRes);
+})
