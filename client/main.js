80ca64173864e328174b1e2c93c5ebf230516480
dastangoo <hamed.dastangoo@gmail.com>
2019-05-07 18:59:28 +0430

    Add some examples

--- client/main.js
+++ client/main.js
@@ -36,6 +36,15 @@ const getApplicationServerKey = () => {
         // Return arrayBuffer as new Uint8Array
         .then(key => new Uint8Array(key))
 }
+// Unsubscribe from push service
+const unsubscribe = () => {
+   // Unsubscribe & update UI
+    swReg.pushManager.getSubscription().then(subscription => {
+        subscription.Unsubscribe().then(() => {
+            setSubscribeStatus(false);
+        })
+    })
+}
 // Subscribe for push Notifications
 const subscribe = () => {
     //console.log('Subscribing');
@@ -44,8 +53,18 @@ const subscribe = () => {
     if (!swReg) return console.error('Service Worker Registration Not Found');
     
     // Get applicationServerKey from push server
-    getApplicationServerKey().then(key => {
-        //swReg.pushManager.subscribe({userVisibleOnly: true, applicationServerKey: publicKey});
+    getApplicationServerKey().then(applicationServerKey => {
+        // Subscribe
+        //console.log(swReg);
+        swReg.pushManager.subscribe({userVisibleOnly: true, applicationServerKey})
+            .then(res => res.toJSON())
+            .then(subscription => {
+                //console.log(subscription);
+                // Pass subscription to server
+                fetch(`${serverUrl}/subscribe`, { method: 'POST', body: JSON.stringify(subscription) })
+                .then(setSubscribeStatus)
+                .catch(unsubscribe);
+            })
     });
     
 
--- server/push.js
+++ server/push.js
@@ -5,6 +5,9 @@ const urlsafeBase64 = require('urlsafe-base64');
 // Vapid keys
 const vapid = require('./vapid.json');
 
+// Subscriptions
+let subscriptons = [];
+
 // Create URL safe vapid public key
 //module.exports.getKey = () => {
     //return urlsafeBase64.decode(vapid.publicKey);
@@ -12,3 +15,11 @@ const vapid = require('./vapid.json');
 
 // ES2015 Short Syntax
 module.exports.getKey = () => urlsafeBase64.decode(vapid.publicKey);
+
+module.exports.addSubscription = (subscription) => {
+    
+    // Add to subscriptions array
+    subscriptions.push(subscription);
+
+    console.log(subscriptions);
+}
