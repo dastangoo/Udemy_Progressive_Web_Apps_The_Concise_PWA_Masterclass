b20a9765dad73acd8058aaf3f8d6b6e20123c797
dastangoo <hamed.dastangoo@gmail.com>
2019-05-07 19:36:01 +0430

    Add some examples

--- server/push.js
+++ server/push.js
@@ -1,12 +1,15 @@
 // Modules
 const webpush = require('web-push');
 const urlsafeBase64 = require('urlsafe-base64');
+const Storage = require('node-storage');
 
 // Vapid keys
 const vapid = require('./vapid.json');
 
 // Subscriptions
-let subscriptons = [];
+const store = new Storage(`${__dirname}/db`);
+let subscriptions = store.get('subscriptions') || [];
+console.log(subscriptions);
 
 // Create URL safe vapid public key
 //module.exports.getKey = () => {
@@ -21,5 +24,8 @@ module.exports.addSubscription = (subscription) => {
     // Add to subscriptions array
     subscriptions.push(subscription);
 
-    console.log(subscriptions);
+    //console.log(subscriptions);
+
+    // Persist subscriptions
+    store.put('subscriptions', subscriptions);
 }
