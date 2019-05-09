a7ceba2cbea78e7aadd0f537ad9a4bf7e42f51a0
dastangoo <hamed.dastangoo@gmail.com>
2019-05-09 13:43:08 +0430

    Camera

--- app/main.js
+++ app/main.js
@@ -1,39 +1,50 @@
+// Init new camera instance on the player node
+//const camera = new Camera(document.getElementById('player'));
+const camera = new Camera($('#player')[0]);
+
 // Main app logic
 const _init = () => {
     //console.log('App running');
 
     // Switch on camera in viewfinder
     $('#viewfinder').on("show.bs.modal", () => {
-        console.log('camera on');     
+        //console.log('camera on');
+        camera.switch_on();
     });
 
     // Switch off camera in viewfinder
     $('#viewfinder').on("hidden.bs.modal", () => {
-        console.log('camera off');     
+        //console.log('camera off');
+        camera.switch_off();
     });
 
     // Take photo
     $('#shutter').on('click', () => {
-        console.log('take photo');
+        //console.log('take photo');
+        let photo = camera.take_photo();
+
+        // Show photo preview in camera button
+        $("#camera").css('background-image', `url(${photo})`).addClass('withphoto');
     });
 
     // Submit Message
     $('#send').on('click', () => {
-       // Get caption text
-       let caption = $('#caption').val();
+        // Get caption text
+        let caption = $('#caption').val();
 
-       // Check message is ok
-       //if (!photo || !caption) {
-       if (!caption) {
-           // Show notification and return
-           toastr.warning('Photo & Caption Required.', 'Incomplete Message');
-           return;
-       }
+        // Check message is ok
+        if (!camera.photo || !caption) {
+            // Show notification and return
+            toastr.warning('Photo & Caption Required.', 'Incomplete Message');
+            return;
+        }
 
-       console.log('adding message');
-       console.log(caption);
+        console.log('adding message');
+        console.log(caption);
 
-       // Reset caption field on success
-       $('#caption').val('');
+        // Reset caption & photo on success
+        $('#caption').val('');
+        $('#camera').css('background-image', '').removeClass('withphoto');
+        camera.photo = null;
     });
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // Static Express server
 const express = require('express');
 const http = require('http');
+//const Camera = require('./../Classes/Camera');
 
 // Create HTTP Server
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.static(`${__dirname}/../app`));
 
 // Server "node_modules" directory
 app.use('/modules', express.static(`${__dirname}/../node_modules`));
+//app.use('/Classes', express.static(`${__dirname}/../Classes`));
 
 // Start Server
 server.listen(8000, () => console.log('Photo Message running on localhost:8000'));
